9e06dedc6aa8374be61157eaca81351ce94c6712
Ivan Astanaev <vanyaastanaev1@gmail.com>
2023-06-02 20:57:03 +0700

    fix catalog-filter

--- src/containers/catalog-filter/index.js
+++ src/containers/catalog-filter/index.js
@@ -46,8 +46,8 @@ function CatalogFilter() {
 
   return (
     <SideLayout padding='medium'>
-      <Select options={options.sort} value={select.sort} onChange={callbacks.onSort}/>
       <Select options={select.categories} value={select.category} onChange={callbacks.onCategory}/>
+      <Select options={options.sort} value={select.sort} onChange={callbacks.onSort}/>
       <Input value={select.query} onChange={callbacks.onSearch} placeholder={'Поиск'}
              delay={1000}/>
       <button onClick={callbacks.onReset}>{t('filter.reset')}</button>
